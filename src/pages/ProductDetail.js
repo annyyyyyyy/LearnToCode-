23f3a9e64f776109070bc92d591849600d80fc53
Aananya <136921254+annyyyyyyy@users.noreply.github.com>
2024-11-28 00:13:13 +0530

    udated env on netlify

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
-import { toast } from "react-toastify";
 import Rating from "../components/Elements/Rating";
 import useTitle from "../hooks/useTitle";
 import { useCart } from "../context";
 import { getProduct } from "../services";
+import { toast } from "react-toastify";
 
 export const ProductDetail = () => {
 
